2e907c55fa687323c0b45907113f6c5b229d5451
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-06-28 12:48:08 +0700

    Hapus Tombol Forget

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -8,7 +8,6 @@ export default function LoginPage() {
         <Input required id="email" label="Email" name="email" type="email" />
         <Input required id="password" label="Password" name="password" type="password" />
         <Button color='primary' type='submit'>Log in</Button>
-        <Button color='primary' type='submit'>Forget ?</Button>
       </form>
     </div>
   
