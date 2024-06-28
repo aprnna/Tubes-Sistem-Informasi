e65bc62ac3cf6a7828d816e5d3457ee922611f10
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-06-28 22:01:02 +0700

    update layout.txt on app and menu route

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,22 +8,25 @@ export default function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
-      <Navbar />
-      <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-        {children}
+    <>
+      <main className="w-full h-screen flex items-center justify-center bg-slate-900">
+          {children}
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-          title="nextui.org homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">NextUI</p>
-        </Link>
-      </footer>
-    </div>
+    </>
+    // <div className="relative flex flex-col h-screen">
+    //   <Navbar />
+    //   <footer className="w-full flex items-center justify-center py-3">
+    //     <Link
+    //       isExternal
+    //       className="flex items-center gap-1 text-current"
+    //       href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+    //       title="nextui.org homepage"
+    //     >
+    //       <span className="text-default-600">Powered by</span>
+    //       <p className="text-primary">NextUI</p>
+    //     </Link>
+    //   </footer>
+    // </div>
+    
   );
 }
