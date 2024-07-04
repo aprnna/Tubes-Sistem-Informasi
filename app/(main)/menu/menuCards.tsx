35363011176675c09ef366cc4d95fb1299b8a707
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-07-04 16:33:22 +0700

    update cards for app/menu

--- app/(main)/menu/menuCards.tsx
+++ app/(main)/menu/menuCards.tsx
@@ -19,9 +19,12 @@ export default function MenuCards(){
         <div className="flex flex-wrap items-center justify-around p-12 gap-8">
             {menu.map((item:any) => (
                 <div key={item.id} className="flex drop-shadow-md">
-                    <div className="flex flex-col bg-white w-[250px] h-auto p-6 rounded-lg items-center gap-4">
-                        <img alt={item.nama} className="border w-full h-[250px] border-amber-800 rounded-xl" src={item.foto} />
-                        <h1>{item.nama}</h1>
+                    <div className="flex flex-col bg-white w-[280px] h-auto p-6 rounded-lg items-center gap-4 hover:bg-red-100 transition-all duration-300 cursor-pointer">
+                        <img alt={item.nama} className="border w-full h-auto max-h-[270px] min-h-[270px] rounded-xl" src={item.foto} />
+                        <div className="flex flex-col justify-center items-center gap-1">
+                            <h1>{item.nama}</h1>
+                            <p className="text-2xl font-bold">Rp. {item.harga}</p>
+                        </div>
                     </div>
                 </div>
             ))}         
