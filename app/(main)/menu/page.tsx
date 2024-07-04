030f9b1c5199bd5aa56de1049d73a47f50c1cc08
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-07-04 16:00:10 +0700

    update design app/menu

--- app/(main)/menu/page.tsx
+++ app/(main)/menu/page.tsx
@@ -6,10 +6,12 @@ import MenuCards from "./menuCards";
 
 export default function AboutPage() {
   return (
-    <div className="w-full h-screen bg-slate-50  flex flex-col">
+    <div className="w-full h-screen bg-slate-50 flex flex-col">
       <TopContent />
       {/* <TableMenu /> */}
-      <MenuCards/>
+      <div className="flex-1 overflow-y-auto">
+        <MenuCards />
+      </div>
     </div>
   );
 }
