04a79887a0e4ac14eab42920842bcb8a7df0b42c
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-07-13 00:00:20 +0700

    add limitation on sidebar display, and add app/bahan_baku

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -19,9 +19,23 @@ const Sidebar = () => {
     { icon: <OrdersIcon />, text: 'Pesanan' , href:'/pesanan'},
     { icon: <OngoingOrders />, text: 'Pesanan Berlangsung' , href:'/pesanan/ongoing'},
     { icon: <ManageMenu />, text: 'Manajemen Menu' , href:'/menu'},
-    { icon: <StorageIcon />, text: 'Bahan Baku' , href:'/'},
+    { icon: <StorageIcon />, text: 'Bahan Baku' , href:'/bahan_baku'},
     { icon: <Laporan />, text: 'Laporan' , href:'/'}
   ];
+
+  const filteredMenuItems = menuItems.filter((item) => {
+    if (pathname === '/menu') {
+      return item.href === '/menu' || item.href === '/pesanan/ongoing';
+    } else if (pathname === '/pesanan' || pathname === '/pesanan/add') {
+      return item.href === '/pesanan/add' || item.href === '/pesanan';
+    } else if (pathname === '/pesanan/ongoing') {
+      return item.href === '/menu' || item.href === '/pesanan/ongoing';
+    } else if (pathname === '/bahan_baku') {
+      return item.href === '/bahan_baku';
+    }
+
+    return true;
+  });
 
   return (
     <div className={`flex flex-col h-screen p-3 bg-white ${isExpanded ? 'w-72' : 'w-24'} transition-all duration-300 drop-shadow-md relative z-50`}>
@@ -32,7 +46,7 @@ const Sidebar = () => {
         {isExpanded ? <BackIcon className='rotate-180' /> : <BackIcon />}
       </button>
       <div className="flex flex-col space-y-4 relative">
-        {menuItems.map((item, index) => (
+        {filteredMenuItems.map((item, index) => (
           <Link
             key={index}
             className={`flex flex-col ${isExpanded ? 'items-start px-3' : 'items-center'} cursor-pointer hover:bg-red-100 hover:text-amber-900 transition-all duration-300 p-3 py-4 rounded-xl relative ${pathname === item.href ?'bg-red-100 text-amber-900':'text-amber-950'}`}
@@ -50,7 +64,7 @@ const Sidebar = () => {
               )}
             </div>
             {!isExpanded && (
-              <div className={`absolute left-full top-1/2 transform -translate-y-1/2 bg-amber-900 text-white shadow-md p-2 px-4 rounded ml-2 z-50 transition-all duration-300 ${hoveredItem === index ? 'opacity-100' : 'opacity-0'}`}>
+              <div className={`absolute left-full top-1/2 transform -translate-y-1/2 bg-amber-900 text-white shadow-md p-2 px-4 rounded ml-2 transition-all duration-300 ${hoveredItem === index ? 'opacity-100 z-50' : 'opacity-0 -z-50'}`}>
                 {item.text}
               </div>
             )}
