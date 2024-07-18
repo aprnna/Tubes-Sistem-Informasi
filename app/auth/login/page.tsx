b9f8c67d2070749d9217115827d72908265cd5a7
aprnna <aprianacahya04@gmail.com>
2024-07-18 23:30:15 +0700

    chore: Update login page UI and add login functionality

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,27 +1,38 @@
-import { Input } from '@nextui-org/input'
-import { Button } from '@nextui-org/button'
-import { title } from '@/components/primitives'
+import { Input } from "@nextui-org/input";
+import { title } from "@/components/primitives";
+import { Button } from "@/components/Button";
 
 export default function LoginPage() {
   return (
-    <div className='grid grid-cols-2 p-4'>
+    <div className="grid grid-cols-2 p-4">
       <div>
-        <img alt='loginImg' src='/loginImg.png' />
+        <img alt="loginImg" src="/loginImg.png" />
       </div>
-      <div className='flex justify-center items-center'>
-        <div  className='max-w-md w-full space-y-5'>
-          <h1 className={title()}>Silahkan Login</h1>
-          <hr className='h-2 bg-black'/>
-          <form action='/api/auth/login' className='space-y-2' method='post'>
-            <Input required id="email" label="Email" name="email" type="email" />
-            <Input required id="password" label="Password" name="password" type="password" />
-            <Button className='w-full' color='primary' type='submit'>Log in</Button>
+      <div className="flex justify-center items-center">
+        <div className="max-w-md w-full space-y-5">
+          <h1 className={title({ color: "coklat" })}>Silahkan Login</h1>
+          <hr className="h-1 bg-black" />
+          <form action="/api/auth/login" className="space-y-2" method="post">
+            <Input
+              required
+              id="email"
+              label="Email"
+              name="email"
+              type="email"
+            />
+            <Input
+              required
+              id="password"
+              label="Password"
+              name="password"
+              type="password"
+            />
+            <Button className="w-full" color="primary" type="submit">
+              Log in
+            </Button>
           </form>
         </div>
-        
       </div>
-      
     </div>
-  
-  )
+  );
 }
