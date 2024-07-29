1c3f830704060fc5636508236a9e63a11015ea52
aprnna <aprianacahya04@gmail.com>
2024-07-29 13:23:50 +0700

    feat: Improve login functionality and error handling

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,8 +2,9 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
+import getResponse from '@/utils/getResponse'
 
 export async function POST(req: NextRequest) {
   const supabase = createClient()
@@ -11,35 +12,40 @@ export async function POST(req: NextRequest) {
   const { data:dataAuth, error } = await supabase.auth.signInWithPassword({
     email: data.get('email') as string,
     password: data.get('password') as string,
-  })
+  })  
 
   if (error) {
     console.error(error)
-    redirect('/error')
+
+    return getResponse(error, 'error login', 400)
   }
   const {data:dataUser, error:errorDataUser} = await supabase.from('users').select().eq('id', dataAuth.user.id).single()
   
   if (errorDataUser) {
     console.error(errorDataUser)
-    redirect('/error')
+
+    return getResponse(errorDataUser, 'error login', 400)
   }
+
+  let redirectTo = '/error';
+
   switch (dataUser.role) {
     case 'manager':
-      redirect('/admin')
+      redirectTo = '/admin'
       break;
     case 'kasir':
-      redirect ('/pesanan/add')
+      redirectTo = '/pesanan/add'
       break;
     case 'koki':
-      redirect('/pesanan/ongoing')
+      redirectTo = '/pesanan/ongoing'
       break;
     case 'karyawan':
-      redirect('/bahan_baku')
+      redirectTo = '/bahan_baku'
       break;
     default:
-      // revalidatePath('/', 'layout')
-      redirect('/error')
+      redirectTo = '/error'
       break;
   }
- 
+
+  return NextResponse.redirect(new URL(redirectTo, req.url))
 }
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,33 @@
+"use client";
 import { Input } from "@nextui-org/input";
 import { title } from "@/components/primitives";
 import { Button } from "@/components/Button";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function LoginPage() {
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      body: formData,
+    });
+
+    setLoading(false);
+    if (response.redirected) {
+      toast.success("Login success");
+      window.location.href = response.url;
+    } else {
+      const result = await response.json();
+
+      toast.error("Login failed");
+      console.error("Login failed:", result.error);
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 p-4">
       <div>
@@ -12,7 +37,7 @@ export default function LoginPage() {
         <div className="max-w-md w-full space-y-5">
           <h1 className={title({ color: "coklat" })}>Silahkan Login</h1>
           <hr className="h-1 bg-black" />
-          <form action="/api/auth/login" className="space-y-2" method="post">
+          <form onSubmit={handleSubmit} className="space-y-2">
             <Input
               required
               id="email"
@@ -27,7 +52,12 @@ export default function LoginPage() {
               name="password"
               type="password"
             />
-            <Button className="w-full" color="primary" type="submit">
+            <Button
+              isLoading={loading}
+              className="w-full"
+              color="primary"
+              type="submit"
+            >
               Log in
             </Button>
           </form>
